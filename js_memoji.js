bbd456ba9522312b0a779c7dd2ea8255388697c8
Lev Demidov <levdemidov@MacBook-Lev.local>
2021-01-17 13:47:05 +0300

    Перемешивание массива

--- js_memoji.js
+++ js_memoji.js
@@ -18,4 +18,18 @@ function coupleArr(array1, array2){
     universalArray = array1.concat(array2);
     return universalArray;
 }
-coupleArr(img, img);
+img = coupleArr(img, img);
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+  
+      // поменять элементы местами
+      // мы используем для этого синтаксис "деструктурирующее присваивание"
+      // подробнее о нём - в следующих главах
+      // то же самое можно записать как:
+      // let t = array[i]; array[i] = array[j]; array[j] = t
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return img
+  }
+img = shuffle(img)
